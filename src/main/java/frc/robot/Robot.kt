6a914441ad1918fee873e753286e027cb948b7f4
falon <falon.n.clark@gmail.com>
2024-12-08 01:07:59 -0500

    bind otf pathplanning

--- src/main/java/frc/robot/Robot.kt
+++ src/main/java/frc/robot/Robot.kt
@@ -1,30 +1,34 @@
 package frc.robot
 
-import edu.wpi.first.math.MathUtil
+import edu.wpi.first.apriltag.AprilTagFieldLayout
+import edu.wpi.first.apriltag.AprilTagFields
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.units.Units.Volts
+import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
-import frc.robot.commands.swerve.AccelerationControlledDriveCommand
 import frc.robot.subsystem.swerve.Drivebase
+import lib.math.units.measuredIn
 import org.littletonrobotics.junction.LogFileUtil
 import org.littletonrobotics.junction.LoggedRobot
 import org.littletonrobotics.junction.Logger
 import org.littletonrobotics.junction.networktables.NT4Publisher
 import org.littletonrobotics.junction.wpilog.WPILOGReader
 import org.littletonrobotics.junction.wpilog.WPILOGWriter
-import kotlin.math.pow
 
 object Robot : LoggedRobot() {
     val driverController = CommandXboxController(0)
 
+    val field = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo)
+
     val drivebase =
         Drivebase().apply {
             defaultCommand =
-                AccelerationControlledDriveCommand(
-                    this,
-                    { -MathUtil.applyDeadband(driverController.leftY, 0.05).pow(3) * Drivebase.maxVelocity },
-                    { -MathUtil.applyDeadband(driverController.leftX, 0.05).pow(3) * Drivebase.maxVelocity },
-                    { -MathUtil.applyDeadband(driverController.rightX, 0.05) * 2 * Math.PI },
-                    { false },
+                robotRelativeDriveCommand(
+                    { -driverController.leftY },
+                    { -driverController.leftX },
+                    { -driverController.rightX },
                 )
         }
 
@@ -52,9 +56,14 @@ object Robot : LoggedRobot() {
         Logger.start()
 
         configureBindings()
+        println((12.0 measuredIn Volts) as Voltage)
     }
 
     private fun configureBindings() {
+        driverController.y().whileTrue(drivebase.getSendToPoseCmd { Pose2d(2.0, 2.0, Rotation2d.kZero) })
+        driverController.a().whileTrue(drivebase.getSendToPoseCmd { Pose2d(2.0, 8.0, Rotation2d.kZero) })
+        driverController.x().whileTrue(drivebase.getSendToPoseCmd { Pose2d(8.0, 2.0, Rotation2d.kZero) })
+        driverController.b().whileTrue(drivebase.getSendToPoseCmd { Pose2d(8.0, 8.0, Rotation2d.kZero) })
     }
 
     override fun robotPeriodic() {
