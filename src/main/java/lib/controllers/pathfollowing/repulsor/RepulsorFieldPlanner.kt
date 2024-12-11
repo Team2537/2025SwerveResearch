1f0798c32e73903029ff91ed17b14d4c133b4983
falon <falon.n.clark@gmail.com>
2024-12-10 22:32:25 -0500

    add in dodge force

--- src/main/java/lib/controllers/pathfollowing/repulsor/RepulsorFieldPlanner.kt
+++ src/main/java/lib/controllers/pathfollowing/repulsor/RepulsorFieldPlanner.kt
@@ -8,6 +8,7 @@ import frc.robot.Robot
 import java.util.Optional
 import java.util.function.Supplier
 import kotlin.math.abs
+import kotlin.math.sign
 
 class RepulsorFieldPlanner {
     private val obstacles = FIELD_OBSTACLES + WALLS
@@ -49,7 +50,7 @@ class RepulsorFieldPlanner {
     fun getForce(position: Translation2d): Force {
         var goalForce = getForceToGoal(position)
         obstacles.forEach { obs ->
-            goalForce += obs.getForceAtPosition(position)
+            goalForce += obs.getForceAtPosition(position, goal)
         }
         return goalForce
     }
@@ -96,15 +97,17 @@ class RepulsorFieldPlanner {
     }
 
     companion object {
-        val GOAL_STRENGTH = 1.0
+        const val GOAL_STRENGTH = 1.0
+        const val STAGE_POINT_STRENGTH = 0.7
+
         val FIELD_OBSTACLES =
             listOf(
-                PointObstacle(Translation2d(5.56, 2.74), 0.7, true),
-                PointObstacle(Translation2d(3.45, 4.07), 0.7, true),
-                PointObstacle(Translation2d(5.56, 5.35), 0.7, true),
-                PointObstacle(Translation2d(11.0, 2.74), 0.7, true),
-                PointObstacle(Translation2d(13.27, 4.07), 0.7, true),
-                PointObstacle(Translation2d(11.0, 5.35), 0.7, true),
+                DodgedPointObstacle(Translation2d(5.56, 2.74), STAGE_POINT_STRENGTH, true),
+                DodgedPointObstacle(Translation2d(3.45, 4.07), STAGE_POINT_STRENGTH, true),
+                DodgedPointObstacle(Translation2d(5.56, 5.35), STAGE_POINT_STRENGTH, true),
+                DodgedPointObstacle(Translation2d(11.0, 2.74), STAGE_POINT_STRENGTH, true),
+                DodgedPointObstacle(Translation2d(13.27, 4.07), STAGE_POINT_STRENGTH, true),
+                DodgedPointObstacle(Translation2d(11.0, 5.35), STAGE_POINT_STRENGTH, true),
             )
 
         val FIELD_LENGTH = Robot.field.fieldLength
@@ -119,7 +122,10 @@ class RepulsorFieldPlanner {
             )
 
         abstract class Obstacle(val strength: Double = 1.0, val positive: Boolean = true) {
-            abstract fun getForceAtPosition(position: Translation2d): Force
+            abstract fun getForceAtPosition(
+                position: Translation2d,
+                target: Translation2d,
+            ): Force
 
             protected fun forceMagnitudeFromDistance(distance: Double): Double {
                 val forceMag = strength / (0.00001 + abs(distance * distance))
@@ -132,11 +138,52 @@ class RepulsorFieldPlanner {
             strength: Double = 1.0,
             positive: Boolean = true,
         ) : Obstacle(strength, positive) {
-            override fun getForceAtPosition(position: Translation2d): Force {
+            override fun getForceAtPosition(
+                position: Translation2d,
+                target: Translation2d,
+            ): Force {
                 return Force(
                     forceMagnitudeFromDistance(loc.getDistance(position)),
                     position.minus(loc).angle,
                 )
+            }
+        }
+
+        class DodgedPointObstacle(
+            val loc: Translation2d,
+            strength: Double = 1.0,
+            positive: Boolean = true,
+        ) : Obstacle(strength, positive) {
+            override fun getForceAtPosition(
+                position: Translation2d,
+                target: Translation2d,
+            ): Force {
+                val dist = loc.getDistance(position)
+                if (dist > 4) return Force()
+                val targetToLoc = loc - target
+                val targetToLocAngle = targetToLoc.angle
+
+                // Place the dodge point 1 meter away from the obstacle opposite the target
+                val dodgePoint = loc + Translation2d(1.0, targetToLocAngle)
+                val sidewaysMag = forceMagnitudeFromDistance(dodgePoint.getDistance(position)) / 2
+                val outwardsMag = forceMagnitudeFromDistance(loc.getDistance(position))
+
+                val initial =
+                    Force(
+                        outwardsMag,
+                        (position - loc).angle,
+                    )
+
+                val sidewaysTheta = (target - position).angle - (position - dodgePoint).angle
+                val sideways = sidewaysMag * sign(sidewaysTheta.sin)
+
+                val sidewaysAngle = targetToLocAngle.rotateBy(Rotation2d.kCCW_90deg)
+
+                return initial +
+                    Force(
+                        sideways,
+                        sidewaysAngle,
+                    )
             }
         }
 
@@ -145,7 +192,10 @@ class RepulsorFieldPlanner {
             strength: Double = 1.0,
             positive: Boolean = true,
         ) : Obstacle(strength, positive) {
-            override fun getForceAtPosition(position: Translation2d): Force {
+            override fun getForceAtPosition(
+                position: Translation2d,
+                target: Translation2d,
+            ): Force {
                 return Force(
                     0.0,
                     forceMagnitudeFromDistance(y - position.y),
@@ -158,7 +208,10 @@ class RepulsorFieldPlanner {
             strength: Double = 1.0,
             positive: Boolean = true,
         ) : Obstacle(strength, positive) {
-            override fun getForceAtPosition(position: Translation2d): Force {
+            override fun getForceAtPosition(
+                position: Translation2d,
+                target: Translation2d,
+            ): Force {
                 return Force(
                     forceMagnitudeFromDistance(x - position.x),
                     0.0,
