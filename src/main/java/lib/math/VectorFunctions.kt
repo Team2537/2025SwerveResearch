eeaa2a3c3f8f92d0d4017bddd67f51e2069b3e5b
falon <falon.n.clark@gmail.com>
2024-12-27 14:59:24 -0500

    Vector<N2>.rotation extension property

--- src/main/java/lib/math/VectorFunctions.kt
+++ src/main/java/lib/math/VectorFunctions.kt
@@ -8,6 +8,9 @@ import edu.wpi.first.math.numbers.N2
 val Rotation2d.vector: Vector<N2>
     get() = VecBuilder.fill(cos, sin)
 
+val Vector<N2>.rotation: Rotation2d
+    get() = Rotation2d(get(0), get(1))
+
 fun rotationFromVector(vec: Vector<N2>): Rotation2d {
     return Rotation2d(vec[0], vec[1])
 }
